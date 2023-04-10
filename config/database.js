fa609179a108012a8b394560d34c3b93311697ad
vivekraj_kr <vivekrajkr.mail@gmail.com>
2023-04-10 20:18:56 +0530

    Update db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env(
+        "DATABASE_HOST",
+        "dpg-cgk4fg8rddleudspq180-a.oregon-postgres.render.com"
+      ),
+      port: env("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "portfolio_2lmo"),
+      user: env("DATABASE_USERNAME", "admin"),
+      password: env("DATABASE_PASSWORD", "rQbGt8uA1Cqh0LGAs1NyjRIKXe3CjY3R"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
     useNullAsDefault: true,
   },
